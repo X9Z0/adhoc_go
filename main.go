2d4643791a4ee3eb865f936412fd148777d75a05
X9Z0 <x9z0cofd@gmail.com>
2025-01-01 08:50:03 +0530

    types

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
 
 func swap(x, y string) (string, string) {
 	return y, x
